4f7b53e251891bb733b3243815423f73c82d3fad
cylixstar <cylixstar@gmail.com>
2011-10-08 01:20:40 +0800

    A Better Dancing Links

--- ACM/DancingLinks.cc
+++ ACM/DancingLinks.cc
@@ -1,288 +1,169 @@
-#include <iostream>
-#include <cstdio>
-#include <vector>
-#include <utility>
-#include <algorithm>
-using namespace std;
+/**
+ * @author cylixstar
+ * 
+ * Dancing Links
+ *
+ * Special thanks to kelvinlau
+ *
+ * @date 2011-10-07
+ *
+ **/
 
-struct DancingLinks
-{
-	int numRow,numCol;
-	DancingLinks(int numRow,int numCol):
-		numRow(numRow),numCol(numCol),
-		colCnt(numCol,0)
-		{}
-	vector<pair<int,int> >nodes;
-	vector<int>rowHead,colHead;
-	vector<int>left,right,up,down;
-	vector<int>colCnt;
-	vector<int>solution;
-	vector<bool> covered;
-	int totNode,base,baseCol,baseRow;
+namespace DancingLinks {
+	const int maxNode = 80000;
+	const int maxRow = 9 * 9 * 9;
+	const int maxCol = 9 * 9 * 9;
+	struct DanceNode{
+		int x,y;
+		DanceNode *l,*r,*u,*d;
+	};
 
-	void addNode(int row,int col)
-	{
-		nodes.push_back(make_pair(row,col));
-	}
+	DanceNode nodes[maxNode];
+	DanceNode *row[maxRow],*col[maxCol];
+	DanceNode *root,*curAva;
+	int cnt[maxCol];
+	//Just for heuristic function
+	//for repetition cover
+	bool covered[maxCol];
 
-	void makeLinks()
-	{
-		baseRow = nodes.size();
-		baseCol = baseRow + numRow;
-		totNode = baseCol + numCol;
+	void init(int numRow,int numCol) {
+		memset(row,0,sizeof(row));
 
-		left.assign(totNode + 1,-1);
-		right.assign(totNode + 1,-1);
-		up.assign(totNode + 1,-1);
-		down.assign(totNode + 1,-1);
-		
-		//For heuristic functino
-		covered.assign(numCol,false);
+		curAva = nodes;
+		root = curAva++;
+		root->l = root->r = root;
+		DanceNode* p;
+		for(int i = 0;i<numCol;i++) {
+			cnt[i] = 0;
+			col[i] = p = curAva++;
+			p->x = -1,p->y = i;
+			p->u = p->d = p;
+			p->r = root;
+			p->l = root->l;
+			p->l->r = p->r->l = p;
 
-		int row,col;
-		int head,tail;
-
-		sort(nodes.begin(),nodes.end());
-		for(int i = 0;i < numRow;i++){
-			if(i < numRow - 1) {
-				down[baseRow + i] = baseRow + i + 1;
-				up[baseRow + i + 1] = baseRow + i;
-			}
-			left[baseRow + i] = right[baseRow + i] = baseRow + i;
-		}
-		up[baseRow] = baseRow + numRow - 1;
-		down[baseRow + numRow - 1] = baseRow;
-
-		for(int i = 0;i < numCol;i++){
-			if(i < numCol - 1) {
-				right[baseCol + i] = baseCol + i + 1;
-				left[baseCol + i + 1] = baseCol + i;
-			}
-			up[baseCol + i] = down[baseCol + i] = baseCol + i;
-		}
-		left[baseCol] = totNode;
-		right[baseCol + numCol - 1] = totNode;
-
-		left[totNode] = baseCol + numCol - 1;
-		right[totNode] = baseCol;
-
-		for(int i = 0 ; i < nodes.size();i++)
-		{
-			row = nodes[i].first;
-			col = nodes[i].second;
-			colCnt[col]++;
-
-			head = baseRow + row;
-			tail = left[head];
-
-			right[tail] = i;
-			left[i] = tail;
-			right[i] = head;
-			left[head] = i;
-
-			head = baseCol + col;
-			tail = up[head];
-
-			down[tail]  = i;
-			up[i] = tail;
-			down[i] = head;
-			up[head] = i;
+			//Just for heuristic function
+			//for repetition cover
+			covered[i] = false;
 		}
 	}
+
 	//Heuristic function
 	//A lower bound of least remain rows
 	//need to select
 	int H() {
 		int ret = 0;
-		static int curNode,iterNode,enumNode,col;
-		for(int i = 0;i<numCol;i++) {
-			covered[i] = false;
-		}
-		for(curNode = right[totNode];curNode != totNode;curNode = right[curNode]) {
-			col = curNode - baseCol;
-			if(!covered[col]) {
+		static DanceNode *cur,*p,*q;
+		for(cur = root->r;cur!=root;cur=cur->r) {
+			if(!covered[cur->y]) {
 				ret++;
-				covered[col] = true;
-				for(iterNode = down[curNode]; iterNode != curNode; iterNode = down[iterNode]) {
-					for(enumNode = right[iterNode]; enumNode != iterNode; enumNode = right[enumNode]) {
-						if(enumNode >= baseRow) continue;
-						covered[nodes[enumNode].second] = true;
+				covered[cur->y];
+				for(p = cur->d;p!=cur;p=p->d) {
+					for(q = p->r;q!=p;q=q->r) {
+						covered[q->y] = true;
 					}
 				}
 			}
 		}
+		for(cur = root->r;cur!=root;cur=cur->r) {
+			covered[cur->y] = false;
+		}
 		return ret;
 	}
+	
+	void add(int x,int y) {
+		DanceNode *p = curAva++;
+		p->x = x,p->y = y;
+		cnt[y]++;
 
-	//if you want to remove a column  which x node is on
-	//remember to invoke by removeColumn(nodes[x].second + baseCol)
-	void removeColumn(int colNode)
-	{
-		static int iterNode,curNode;
-		for(curNode = down[colNode];
-				curNode!=colNode;
-				curNode = down[curNode]) {
-			if(curNode < baseCol) {
-				for(iterNode = right[curNode];iterNode!=curNode;iterNode = right[iterNode]) {
-					if(iterNode >= baseRow) continue;
-					down[up[iterNode]] = down[iterNode];
-					up[down[iterNode]] = up[iterNode];
-					colCnt[nodes[iterNode].second]--;
-				}
-			} else {
-				left[right[curNode]] = left[curNode];
-				right[left[curNode]] = right[curNode];
+		if(!row[x]) {
+			row[x] = p->l = p->r = p;
+		} else {
+			p->l = row[x]->l;
+			p->r = row[x];
+			p->l->r = p->r->l = p;
+		}
+
+		p->u = col[y]->u;
+		p->d = col[y];
+		p->u->d = p->d->u = p;
+	}
+	
+	void del_col_rep(DanceNode* p) {
+		for(DanceNode* q = p->d;q!=p;q=q->d) {
+			q->l->r = q->r;
+			q->r->l = q->l;
+		}
+	}
+	void undel_col_rep(DanceNode* p) {
+		for(DanceNode* q = p->u;q!=p;q = q->u) {
+			q->l->r = q->r->l = q;
+		}
+	}
+
+	void del_col(int y) {
+		DanceNode *p = col[y];
+		p->l->r = p->r;
+		p->r->l = p->l;
+		for(DanceNode* a = p->d;a!=p;a = a->d) {
+			for(DanceNode* b = a->r;b!=a;b = b->r) {
+				b->u->d = b->d;
+				b->d->u = b->u;
+				cnt[b->y]--;
+			}
+		}
+	}
+
+	void undel_col(int y) {
+		DanceNode *p = col[y];
+		p->l->r = p->r->l = p;
+
+		for(DanceNode* a = p->u;a!=p;a = a->u) {
+			for(DanceNode* b = a->l;b!=a;b = b->l) {
+				b->u->d = b->d->u = b;
+				cnt[b->y]++;
 			}
 		}
 	}
 	
-	void restoreColumn(int colNode)
-	{
-		static int iterNode,curNode;
-		for(curNode = up[colNode];
-				curNode!=colNode;
-				curNode = up[curNode]) {
-			if(curNode < baseCol) {
-				for(iterNode = right[curNode];iterNode!=curNode;iterNode = right[iterNode]) {
-					if(iterNode >= baseRow) continue;
-					down[up[iterNode]] = iterNode;
-					up[down[iterNode]] = iterNode;
-					colCnt[nodes[iterNode].second]++;
-				}
-			} else {
-				left[right[curNode]] = curNode;
-				right[left[curNode]] = curNode;
-			}
-		}
-	}
-
-	void removeColumnRep(int colNode) {
-		static int curNode;
-		for(curNode = down[colNode];curNode!=colNode;curNode = down[curNode]) {
-			right[left[curNode]] = right[curNode];
-			left[right[curNode]] = left[curNode];
-		}
-	}
-
-	void restoreColumnRep(int colNode)
-	{
-		static int curNode;
-		for(curNode = up[colNode];curNode!=colNode;curNode = up[curNode]) {
-			right[left[curNode]] = curNode;
-			left[right[curNode]] = curNode;
-		}
-	}
-
-	void search()
-	{
-		if(right[totNode]==totNode)
-		{
-			//handle solution
-			/*for(int i = 0;i<solution.size();i++)
-				printf("%d ",solution[i]);
-			puts("");*/
+	//When dealing with repetition cover
+	//remove *_col function 
+	//then put on *_col_rep function
+	void search() {
+		if(root->r==root) {
+			//handle a solution;
 			return;
 		}
-		int minNode = -1,minCnt = 0,curNode;
-		int iterNode;
-		for(curNode = right[totNode];
-				curNode!=totNode;
-				curNode = right[curNode])
-		{
-			if(minNode < 0 || (minCnt > colCnt[curNode - baseCol]))
-			{
-				minNode = curNode;
-				minCnt = colCnt[curNode - baseCol];
-			}
-		}
-		if(minCnt==0)return;
 
+		//Add a branch cut such as
+		//if(H() + dep > maxstep) then return
+
+		DanceNode *p,*q,*y = root->r;
+		for(p = y->r;p!=root;p = p->r)
+			if(cnt[p->y] < cnt[y->y]) y = p;
 		
-		for(curNode = down[minNode];
-				curNode!=minNode;
-				curNode = down[curNode])
-		{
-			removeColumn(curNode);
-			//removeColumnRep(curNode);
-			for(iterNode = right[curNode];iterNode != curNode;iterNode = right[iterNode]) {
-				if(iterNode < baseRow) {
-					removeColumn(iterNode);
-				}
-				//Replace code in this block if you want to 
-				//solve rep cover problem
-				/*if(iterNode >= baseRow)continue;
-				  removeColumnRep(iterNode);
-				  up[down[iterNode]] = up[iterNode];
-				  down[up[iterNode]] = down[iterNode];
-				  */
+		if(!cnt[y->y])return;
+		
+
+		del_col(y->y);
+		for(p = y->d;p!=y;p = p->d) {
+			//del_col_rep(p);
+			for(q = p->r;q!=p;q = q->r) {
+				del_col(q->y);
+				//del_col_rep(q);
 			}
-			solution.push_back(nodes[curNode].first);
-			//Put some branches-cut code here
+			
+			//put p->x in solution
 			search();
-			solution.pop_back();
-			for(iterNode = left[curNode];iterNode != curNode;iterNode = left[iterNode]) {
-				if(iterNode < baseRow) {
-					restoreColumn(iterNode);
-				}	
-				//Replace code in this block if you want to 
-				//solve rep cover problem
-				/*if(iterNode >= baseRow)continue;
-				  up[down[iterNode]] = iterNode;
-				  down[up[iterNode]] = iterNode;
-				  restoreColumnRep(iterNode);
-				  */
+			//pop p->x out of solution
+
+			for(q = p->l;q!=p;q = q->l) {
+				undel_col(q->y);
+				//undel_col_rep(q);
 			}
-			restoreColumn(curNode);
-			//restoreColumnRep(curNode);
+			//undel_col_rep(p);
 		}
+		undel_col(y->y);
 	}
 };
 
-
-void printrow(const DancingLinks& dlx,int start)
-{
-	int curNode = dlx.right[start];
-	printf("Row %d ",start);
-	for(;curNode!=start;curNode = dlx.right[curNode])
-	{
-		printf("%d ",curNode);
-	}
-	puts("");
-}
-
-void printcol(const DancingLinks& dlx,int start)
-{
-	int curNode = dlx.down[start];
-	printf("Col %d ",start);
-	for(;curNode!=start;curNode = dlx.down[curNode])
-	{
-		printf("%d ",curNode);
-	}
-	puts("");
-}
-
-int main()
-{
-	int numNode,x,y;
-	int numRow,numCol;
-	scanf("%d%d%d",&numRow,&numCol,&numNode);
-	printf("%d %d\n",numRow,numCol);
-	DancingLinks dlx(numRow,numCol);
-	for(int i = 0;i<numNode;i++)
-	{
-		scanf("%d%d",&x,&y);
-		dlx.addNode(x,y);
-	}
-	dlx.makeLinks();
-	printrow(dlx,dlx.totNode);
-	for(int i = 0;i<numRow;i++)
-	{
-		printrow(dlx,dlx.baseRow + i);
-	}
-	for(int i = 0;i<numCol;i++)
-	{
-		printcol(dlx,dlx.baseCol + i);
-	}
-	dlx.search();
-	return 0;
-}
